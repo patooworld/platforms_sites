6bec750eb0dbd07ba4974ffa96b90ab3375d7d5a
PATOOWORLD <samuel@patooworld.com>
2024-06-27 05:21:44 -0700

    passing

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-import { authOptions } from "@/lib/auth";
+import [Nextauth]   from 'next-auth'
 import NextAuth from "next-auth";
 
-const handler = NextAuth(authOptions);
-
+const handler =process.env.NEXTAUTH_ROOT_URL=`,`//api/auth/callback/gh_username
 export { handler as GET, handler as POST };
